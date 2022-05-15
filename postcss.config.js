55a548a5b86b3b8617e334149e59c3c819d0efbb
SamDev7 <66446458+SamDev-7@users.noreply.github.com>
2022-05-15 13:32:22 +0000

    Large Update

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+  plugins: [
+		require('postcss-import'),
+		purgecss({
+    	content: ["./**/*.{html,js}"]
+  	}),
+		require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'default',
+    })
+  ],
+	map: false
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 module.exports = {
+  mode: 'jit',
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        code: ['Source Code Pro', 'monospace'],
+        'poppins': ["'Poppins'", 'sans-serif']
       },
-    },
+      screens: {
+        '-sm': {'max': '640px'},
+        '-md': {'max': '768px'},
+        '-lg': {'max': '1024px'},
+        '-xl': {'max': '1280px'},
+        '-2xl': {'max': '1536px'},
+      }
+    }
   },
+  variants: {},
   plugins: [],
 }
+
+// https://play.tailwindcss.com/4giqp0pOAB?layout=horizontal
+// npx tailwindcss -i ./tailwind.css -o ./main.css
+// npx postcss ./tailwind.css -o ./main.css
