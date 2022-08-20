78f6093b6fdd8b43211e30160afb44f820067bd0
SamDev-7 <hello@saml.is-a.dev>
2022-08-20 16:20:58 -0400

    Theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         '-lg': {'max': '1024px'},
         '-xl': {'max': '1280px'},
         '-2xl': {'max': '1536px'},
+      },
+      animation: {
+        'bounce-slow': 'bounce 2s infinite',
       }
     }
   },
