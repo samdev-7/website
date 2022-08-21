ff69dec3b5c2d48314e71d68e8a85ebac31768d2
SamDev-7 <hello@saml.is-a.dev>
2022-08-21 00:05:56 -0400

    Fixed it!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script>
 	import Navbar from '$lib/header/Navbar.svelte';
 	import '../app.css';
+
+	import theme from '$lib/stores/theme'; 
+	$: currentTheme = $theme === 'dark' ? 'dark' : '';
+	console.log($theme)
+
 </script>
 
-<header>
+<header class="{currentTheme}">
 	<Navbar />
 </header>
 
