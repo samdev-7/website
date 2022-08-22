df9a1b8bb17a5cb8568909d5fb09592781734672
SamDev-7 <hello@saml.is-a.dev>
2022-08-22 12:36:35 -0400

    Update About me

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,16 @@
 	import '../app.css';
 
 	import Navbar from '$lib/Navbar.svelte';
-
+	import Footer from '$lib/Footer.svelte';
+	
+	import { onMount } from 'svelte';
 	import { scroll } from '$lib/scroll';
 
-	function scrollHandler(event) {
+	function scrollHandler() {
 		scroll.set(window.scrollY);
 	}
+
+	onMount(scrollHandler)
 
 </script>
 
@@ -17,8 +21,13 @@
 
 <main class="bg-white dark:bg-slate-900">
 	<slot />
+
+	<div class="py-16 text-center">
+		<p class="text-lg">I'm upgrading this site!</p>
+		<p>It'll be back soon™️</p>
+	</div>
 </main>
 
 <footer class="bg-white dark:bg-slate-900">
-	<!--<Footer />-->
+	<Footer />
 </footer>
