430268d83e9ac416e2bda6a75ce6a05d0e8c693a
SamDev-7 <hello@saml.is-a.dev>
2022-08-30 23:00:29 -0400

    Add 404 Page

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,16 @@
+<div class="w-full -mb-16 h-screen flex">
+
+    <div class="m-auto text-center">
+        <div>
+            <h1 class="text-blue-600 dark:text-blue-400 text-9xl font-medium">404</h1>
+            <h2 class="text-xl">Uh oh! This page could not be found.</h2>
+        </div>
+        <div class="mt-12">
+            <a class="text-blue-600 dark:text-blue-400 hover:underline" rel="noopener" href="/">
+                <svg class="inline h-5 mb-0.5" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 12l2-2m0 0l7-7l7 7M5 10v10a1 1 0 0 0 1 1h3m10-11l2 2m-2-2v10a1 1 0 0 1-1 1h-3m-6 0a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1m-6 0h6"/></svg>
+                <p class="inline">Head Home</p>
+            </a>
+        </div>
+    </div>
+
+</div>
