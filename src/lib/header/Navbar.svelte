c23ab7cbf7fe433afd6af594ff73073f029f28db
SamDev-7 <hello@samliu.dev>
2022-12-22 20:42:04 -0500

    Add snow! ❄️ https://mannes.tech/svelte-snowfall/

--- src/lib/header/Navbar.svelte
+++ src/lib/header/Navbar.svelte
@@ -1,5 +1,8 @@
 <script>
 	import { scroll, scrollToAnchor, scrollToHome } from '$lib/utils/scroll';
+	import { browser } from '$app/env';
+
+	export let snowEnabled = true;
 
 	function toggleTheme() {
 		let theme = localStorage.theme || 'light';
@@ -11,15 +14,36 @@
 			localStorage.theme = 'light';
 		}
 	}
+
+	function toggleSnow() {
+		let theme = localStorage.snow || 'on';
+		if (theme === 'on') {
+			localStorage.snow = 'off';
+			snowEnabled = false;
+		} else {
+			localStorage.snow = 'on';
+			snowEnabled = true;
+		}
+	}
+
+	if (browser) {
+		let snow = localStorage.snow || 'on';
+		if (snow === 'off') {
+			snowEnabled = false;
+		}
+	}
 </script>
 
 <!-- Navbar -->
 <nav
-	class="sticky top-0 z-30 flex h-16 transition-shadow sm:px-16 2xs:-sm:px-8 -2xs:px-0 bg-white dark:bg-slate-900 {$scroll > 5 ? 'shadow' : ''}"
+	class="sticky top-0 z-30 flex h-16 transition-shadow sm:px-16 2xs:-sm:px-8 -2xs:px-0 bg-white dark:bg-slate-900 {$scroll >
+	5
+		? 'shadow'
+		: ''}"
 >
 	<!-- Navbar Brand -->
 	<div class="flex my-auto text-blue-600 dark:text-blue-400 -2xs:hidden">
-		<a rel="noopener"class="flex" href="/" on:click={(event) => scrollToHome(event)}>
+		<a rel="noopener" class="flex" href="/" on:click={(event) => scrollToHome(event)}>
 			<svg
 				class="-mt-0.5"
 				xmlns="http://www.w3.org/2000/svg"
@@ -48,32 +72,61 @@
 		<ul
 			class="space-x-8 font-medium 2xs:-xs:space-x-4 -2xs:space-x-2 -2xs:text-sm text-slate-800 dark:text-slate-200"
 		>
-			<li class="inline"><a
-				class="hover:text-blue-600 dark:hover:text-blue-400"
-				href="/#about"
-				on:click={(event) => scrollToAnchor(event, 'about')}>About</a
-			></li>
-			<li class="inline"><a
-				class="hover:text-blue-600 dark:hover:text-blue-400"
-				href="/#projects"
-				on:click={(event) => scrollToAnchor(event, 'skills')}>Skills</a
-			></li>
-			<li class="inline"><a
-				class="hover:text-blue-600 dark:hover:text-blue-400"
-				href="/#scrapbook"
-				on:click={(event) => scrollToAnchor(event, 'projects')}>Projects</a
-			></li>
-			<li class="inline"><a
-				class="hover:text-blue-600 dark:hover:text-blue-400"
-				href="/#scrapbook"
-				on:click={(event) => scrollToAnchor(event, 'scrapbook')}>Scrapbook</a
-			></li>
-			</ul>
+			<li class="inline">
+				<a
+					class="hover:text-blue-600 dark:hover:text-blue-400"
+					href="/#about"
+					on:click={(event) => scrollToAnchor(event, 'about')}>About</a
+				>
+			</li>
+			<li class="inline">
+				<a
+					class="hover:text-blue-600 dark:hover:text-blue-400"
+					href="/#projects"
+					on:click={(event) => scrollToAnchor(event, 'skills')}>Skills</a
+				>
+			</li>
+			<li class="inline">
+				<a
+					class="hover:text-blue-600 dark:hover:text-blue-400"
+					href="/#scrapbook"
+					on:click={(event) => scrollToAnchor(event, 'projects')}>Projects</a
+				>
+			</li>
+			<li class="inline">
+				<a
+					class="hover:text-blue-600 dark:hover:text-blue-400"
+					href="/#scrapbook"
+					on:click={(event) => scrollToAnchor(event, 'scrapbook')}>Scrapbook</a
+				>
+			</li>
+		</ul>
 
 		<div class="ml-8 mr-6 border-l border-slate-300 dark:border-slate-500 -md:hidden" />
 
 		<!-- Navbar Utils -->
 		<div class="flex space-x-3 text-slate-500 dark:text-slate-400 -md:hidden">
+			<button
+				class="hover:text-slate-800 dark:hover:text-slate-200"
+				title="Toggle Snow"
+				on:click={toggleSnow}
+			>
+				{#if snowEnabled}
+					<svg width="24" height="24" viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="m20.79 13.95l-2.33.62l-2-1.13v-2.88l2-1.13l2.33.62l.52-1.93l-1.77-.47l.46-1.77l-1.93-.52l-.62 2.33l-2 1.13L13 7.38V5.12l1.71-1.71L13.29 2L12 3.29L10.71 2L9.29 3.41L11 5.12v2.26L8.5 8.82l-2-1.13l-.58-2.33L4 5.88l.47 1.77l-1.77.47l.52 1.93l2.33-.62l2 1.13v2.89l-2 1.13l-2.33-.62l-.52 1.93l1.77.47L4 18.12l1.93.52l.62-2.33l2-1.13L11 16.62v2.26l-1.71 1.71L10.71 22L12 20.71L13.29 22l1.41-1.41l-1.7-1.71v-2.26l2.5-1.45l2 1.13l.62 2.33l1.88-.51l-.47-1.77l1.77-.47l-.51-1.93M9.5 10.56L12 9.11l2.5 1.45v2.88L12 14.89l-2.5-1.45v-2.88Z"
+						/></svg
+					>
+				{:else}
+					<svg width="24" height="24" viewBox="0 0 24 24"
+						><path
+							fill="currentColor"
+							d="M11 5.12L9.29 3.41L10.71 2L12 3.29L13.29 2l1.42 1.41L13 5.12v2.26l2.45 1.44l2-1.13l.62-2.33l1.93.52l-.46 1.77l1.77.47l-.52 1.93l-2.33-.62l-2 1.13v2.7L14.5 11.3v-.74l-1.76-1.02l-2.01-2.01l.27-.15V5.12m7.46 9.45l-1.59-.9l2.68 2.68l1.75-.47l-.51-1.93l-2.33.62M13 16.62v2.26l1.7 1.71L13.29 22L12 20.71L10.71 22l-1.42-1.41L11 18.88v-2.26l-2.45-1.44l-2 1.13l-.62 2.33L4 18.12l.47-1.76l-1.77-.47l.52-1.93l2.33.62l2-1.13v-2.89l-2-1.13l-2.33.62l-.52-1.93l1.77-.47L4 5.89L1.11 3l1.28-1.27l19.72 19.73l-1.27 1.27L14.1 16l-1.1.62m-1-1.73l.63-.39l-3.13-3.11v2.05l2.5 1.45Z"
+						/></svg
+					>
+				{/if}
+			</button>
 			<button
 				class="hover:text-slate-800 dark:hover:text-slate-200"
 				title="Toggle Themes"
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,130 @@
 <script>
 	import '../app.css';
 
+	import { cubicOut, backIn } from 'svelte/easing'
+	import { fade } from 'svelte/transition'
+	import { onMount } from 'svelte'
+
 	import Navbar from '$lib/header/Navbar.svelte';
 	import Footer from '$lib/footer/Footer.svelte';
-
+	
 	import { scroll } from '$lib/utils/scroll';
 
 	function scrollHandler() {
 		scroll.set(window.scrollY);
 	}
 
+	let snowEnabled = true; 
+	$: console.log(snowEnabled);
+
+	// Snowfall from https://mannes.tech/svelte-snowfall/
+	// a bunch of variables defining the snow and how it falls
+	const SNOWFLAKES_COUNT = 50
+	const SNOWFLAKE_MIN_SCALE = 0.4
+	const MAX_FALL_DURATION = 10000
+	const MELTING_WAIT = 0
+	const MELTING_DURATION = 2000
+	const WIND_FORCE = 0.1
+	const SNOW_ICONS = ['❆', '❅', '❄']
+	const MAX_TOTAL_TIME = MAX_FALL_DURATION + MELTING_WAIT + MELTING_DURATION
+	// this function generates the random configuration with all necessary values
+	function randomSnowflakeConfig(i) {
+		const scale = SNOWFLAKE_MIN_SCALE + Math.random() * (1 - SNOWFLAKE_MIN_SCALE)
+		const xStart = Math.random() * (1 + WIND_FORCE) - WIND_FORCE
+		return {
+		visible: false,
+		scale,
+		inDelay: Math.random() * MAX_TOTAL_TIME,
+		inDuration: (1 + SNOWFLAKE_MIN_SCALE - scale) * MAX_FALL_DURATION,
+		xStart,
+		xEnd: xStart + scale * WIND_FORCE,
+		snowIcon: SNOW_ICONS[i % SNOW_ICONS.length],
+		}
+	}
+	// actially generating the snowflakes
+	let snowflakes = new Array(SNOWFLAKES_COUNT)
+		// @ts-ignore
+		.fill()
+		.map((_, i) => randomSnowflakeConfig(i))
+		.sort((a, b) => a.scale - b.scale)
+	// the custom fall transition.
+	// See docs for how to create custom Svelte transitions: https://svelte.dev/docs#transition_fn
+	function fall(_node, { delay = 0, duration, xStart = 0.1, xEnd = 0.5, scale = 1.0 }) {
+		return {
+		duration,
+		delay,
+		css: t => {
+			const x_t = backIn(t)
+			const y_t = t
+
+			const x_coord = (xEnd - xStart) * x_t + xStart
+			return `
+			transform: scale(${scale}) rotate(${x_t * 720}deg);
+			left: ${x_coord * 100}%;
+			bottom: ${100 - y_t * 100}%;
+			`
+		},
+		}
+	}
+	// start everything on mount. starting means setting the snowflakes visible.
+	// this "hack" is not needed when you configure your svelte to display transitions on first render:
+	// https://svelte.dev/docs#Client-side_component_API - set `intro: true`
+	onMount(async () => {
+		setTimeout(() => {
+		snowflakes = snowflakes.map(sf => ({ ...sf, visible: true }))
+		}, 50)
+	})
 </script>
 
 <svelte:window on:scroll={scrollHandler} />
 
-<Navbar />
+<!-- Snowfall from https://mannes.tech/svelte-snowfall/ -->
+<style>
+	.snowframe {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: hidden;
+	width: 100%;
+	height: 100%;
+  }
+
+  .snowflake {
+    color: #fff;
+    font-size: 1.2rem;
+    line-height: 1.2rem;
+    font-family: Arial, sans-serif;
+    text-shadow: 0 0 5px #000;
+    position: absolute;
+    z-index: 1000;
+    overflow: hidden;
+  }
+</style>
+
+{#if snowEnabled}
+<div class="snowframe x-40" aria-hidden="true">
+	{#each snowflakes as flake}
+	  {#if flake.visible}
+		<div
+		  class="snowflake"
+		  style={`transform: scale(${flake.scale}); left: ${flake.xEnd * 100}%`}
+		  in:fall={{ delay: flake.inDelay, duration: flake.inDuration, scale: flake.scale, xStart: flake.xStart, xEnd: flake.xEnd }}
+		  out:fade={{ delay: MELTING_WAIT, duration: MELTING_DURATION, easing: cubicOut }}
+		  on:introend={() => (flake.visible = false)}
+		  on:outroend={() => (flake.visible = true)}>
+		  {flake.snowIcon}
+		</div>
+	  {/if}
+	{/each}
+</div>
+{/if}
+
+<Navbar bind:snowEnabled={snowEnabled}/>
 
 <main class="bg-white dark:bg-slate-900">
-	<slot />
+	<slot/>
 </main>
 
 <footer class="bg-white dark:bg-slate-900">
