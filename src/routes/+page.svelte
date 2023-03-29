d21a36e3d613565363e386fda5e5e9f19a5a06d6
Sam Liu <hello@samliu.dev>
2023-03-28 22:37:16 -0400

    Clean up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <div class="w-screen h-screen flex">
     <div class="m-auto">
         <div style="--cursor-color: #2563eb; --cursor-width: 0.20ch">
-            <Typewriter mode="loopRandom" interval={50} unwriteInterval={20} wordInterval={5000}>
+            <Typewriter mode="loopRandom" interval={75} unwriteInterval={20} wordInterval={5000}>
                 {#each greetings as greeting}
                     <h2 class="text-blue-600 text-3xl">{greeting}</h2>
                 {/each}
--- src/routes/api/random/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET = (() => {
-    return json({
-        random: Math.random()
-    })
-}) satisfies RequestHandler;
