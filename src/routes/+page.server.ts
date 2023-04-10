8dd865fcc32a023654e1150ed5b04a2af79316dd
Sam Liu <hello@samliu.dev>
2023-04-10 03:20:30 -0400

    Add DB for caching

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,53 +1,21 @@
 import type { PageServerLoad } from './$types';
-
-import { GITHUB_PAT } from '$env/static/private';
-
-const headers = {
-  "Accept": "application/vnd.github+json",
-  "X-GitHub-Api-Version": "2022-11-28",
-  "Authorization": `Bearer ${GITHUB_PAT}`
-} 
+import { statsCol } from '$lib/mongo';
 
 export const load = (async ({ fetch }) => {
+  const record = await statsCol.findOne({ source: "github" })
+
+  if (!record) {
+    const res = await fetch('/api/cron/gh-stats');
+    const data = await res.json();
+
+    return data;
+  }
+
   const result = {
-    "total_commits": await getTotalCommits(fetch),
-    "total_files": await getTotalFiles(fetch),
-    "total_repos": await getRepos(fetch),
+    "total_commits": record.total_commits,
+    "total_files": record.total_files,
+    "total_repos": record.total_repos,
   }
 
   return result;
 }) satisfies PageServerLoad;
-
-async function getTotalCommits(fetch: { (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>; (arg0: string, arg1: { headers: { Accept: string; "X-GitHub-Api-Version": string; Authorization: string; }; }): unknown; }): Promise<number> {
-  const res = await fetch('https://api.github.com/search/commits?q=author:SamDev-7&per_page=1', { headers });
-
-  if (!res.ok) {
-    return -1;
-  }
-
-  const data = await res.json();
-  return data.total_count;
-}
-
-async function getTotalFiles(fetch: { (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>; (arg0: string, arg1: { headers: { Accept: string; "X-GitHub-Api-Version": string; Authorization: string; }; }): unknown; }): Promise<number> {
-  const res = await fetch('https://api.github.com/search/code?q=user:SamDev-7&per_page=1', { headers });
-
-  if (!res.ok) {
-    return -1;
-  }
-
-  const data = await res.json();
-  return data.total_count;
-}
-
-async function getRepos(fetch: { (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>; (arg0: string, arg1: { headers: { Accept: string; "X-GitHub-Api-Version": string; Authorization: string; }; }): unknown; }): Promise<number> {
-  const res = await fetch('https://api.github.com/user/repos', { headers });
-
-  const data = await res.json()
-
-  if (!res.ok) {
-    return -1;
-  }
-
-  return data.length;
-}
