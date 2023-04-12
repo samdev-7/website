a5eee91bacd447997f29708c2e30e08b383e6f03
Sam Liu <hello@samliu.dev>
2023-04-11 20:46:26 -0400

    Respect reduce-motion for scrolling

--- src/lib/scroll.ts
+++ src/lib/scroll.ts
@@ -1,12 +1,23 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 export const scroll = writable(0);
+let prefersReducedMotion = false;
 
 export function scrollToAnchor(event: MouseEvent, elementId: string) {
+    if (browser) {
+        prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+    }
+
     const element = document.getElementById(elementId);
     if (element) {
         event.preventDefault();
-        element.scrollIntoView({ behavior: 'smooth' });
+
+        if (prefersReducedMotion) {
+            element.scrollIntoView({ behavior: 'auto' });
+        } else {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
     }
 }
 
