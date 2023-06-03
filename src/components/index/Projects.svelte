661699f7da9c250122f13fd3a245b258974d656e
Sam Liu <hello@samliu.dev>
2023-06-02 23:10:38 -0400

    Add AngelHacks Toronto to projects

--- src/components/index/Projects.svelte
+++ src/components/index/Projects.svelte
@@ -1,11 +1,118 @@
 <script lang="ts">
 </script>
 
-<div id="projects" class="scroll-m-16 h-screen">
-	<div class="text-center space-y-4">
-		<h4 class="text-5xl font-medium tracking-wide text-center text-blue-600 -2xs:text-4xl">
-			Projects
-		</h4>
-		<p class="text-lg text-gray-800">Stuff I have done or are still working on!</p>
+<div id="projects" class="scroll-m-16 flex justify-center">
+	<div class="sm:mx-10 lg:mx-20 mx-0 max-w-7xl space-y-16 py-24">
+		<!-- Section -->
+		<div class="text-center space-y-4">
+			<h4 class="text-5xl font-medium tracking-wide text-center text-blue-600 -2xs:text-4xl">
+				Projects
+			</h4>
+			<p class="text-lg text-gray-800">Stuff I have done or are still working on!</p>
+		</div>
+
+		<!-- Project List-->
+		<div class="grid grid-cols-1">
+			<!-- AngelHacks Toronto-->
+			<div
+				class="w-full grid xl:grid-cols-2 bg-[#b2e9ff] p-6 py-10 lg:p-12 sm:rounded-xl border-[#8EDDFD] border-t-2 border-b-2 sm:border-2 gap-x-12"
+			>
+				<div class="flex flex-col">
+					<div class="space-y-8">
+						<!-- Branding -->
+						<div class="flex flex-row space-x-6">
+							<!-- Icon -->
+							<div>
+								<svg
+									width="75"
+									height="75"
+									viewBox="0 0 125 106"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									class=""
+								>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M88.0468 18.6666C87.395 18.8221 86.7035 18.9732 85.9746 19.1195C78.0173 20.7164 65.5969 21.7436 51.641 21.7436C37.6851 21.7436 25.2647 20.7164 17.3075 19.1195C16.5785 18.9732 15.8871 18.8221 15.2353 18.6666C10.7576 17.5984 8.15385 16.3211 8.15385 14.9487C8.15385 13.5764 10.7576 12.2991 15.2353 11.2308C15.8871 11.0753 16.5785 10.9242 17.3075 10.7779C25.2647 9.18103 37.6851 8.15385 51.641 8.15385C65.5969 8.15385 78.0173 9.18103 85.9746 10.7779C86.7035 10.9242 87.395 11.0753 88.0468 11.2308C92.5245 12.2991 95.1282 13.5764 95.1282 14.9487C95.1282 16.3211 92.5245 17.5984 88.0468 18.6666ZM83.6499 27.8095C75.249 29.1221 63.9364 29.8974 51.641 29.8974C39.3456 29.8974 28.033 29.1221 19.6322 27.8095C15.4958 27.1632 11.6941 26.3314 8.74575 25.2422C7.31807 24.7148 5.60021 23.9527 4.08892 22.7987C2.74819 21.775 0 19.2078 0 14.9487C0 10.6896 2.74819 8.12246 4.08892 7.09871C5.60021 5.94473 7.31807 5.18263 8.74575 4.65521C11.6941 3.56604 15.4958 2.73423 19.6322 2.08792C28.033 0.775288 39.3456 0 51.641 0C63.9364 0 75.249 0.775288 83.6499 2.08792C87.7863 2.73423 91.5879 3.56604 94.5363 4.65521C95.964 5.18263 97.6818 5.94473 99.1931 7.09871C100.534 8.12246 103.282 10.6896 103.282 14.9487C103.282 19.2078 100.534 21.775 99.1931 22.7987C97.6818 23.9527 95.964 24.7148 94.5363 25.2422C91.5879 26.3314 87.7863 27.1632 83.6499 27.8095Z"
+										fill="#FFB400"
+									/>
+									<path
+										d="M21.4786 62.5112C9.59205 62.3693 0 52.6894 0 40.7692H72.7051C90.7181 40.7692 105.321 55.3716 105.321 73.3846C105.321 84.5948 99.6649 94.4841 91.0513 100.355V106H69.9872C58.2059 106 48.613 96.6301 48.254 84.9359H48.2436C34.8548 84.9359 23.7272 75.255 21.4786 62.5112Z"
+										fill="#004F58"
+									/>
+									<path
+										d="M19.0256 40.7692C19.0256 52.6894 28.6177 62.3693 40.5042 62.5112C42.7528 75.255 53.8805 84.9359 67.2692 84.9359C67.2727 84.9359 67.2762 84.9359 67.2796 84.9359C67.6386 96.6301 77.2315 106 89.0128 106H91.7308C109.744 106 124.346 91.3976 124.346 73.3846C124.346 55.3716 109.744 40.7692 91.7308 40.7692H19.0256Z"
+										fill="#009AAB"
+									/>
+								</svg>
+							</div>
+							<!-- Project headings -->
+							<div class="space-y-1">
+								<p class="text-4xl font-medium tracking-wide">AngelHacks Toronto</p>
+								<p class="text-2xl text-gray-800">Organizer</p>
+							</div>
+						</div>
+						<!-- Description -->
+						<div>
+							<p class="text-lg">
+								On May 27, 2023, 50 teenagers across Eastern Canada gathered in downtown Toronto for
+								a coding event for students, by students. AngelHacks Toronto was a 12 hour
+								hackathon/game-jam running alongside 2 other AngelHacks events with over 200
+								participants across North America. All organization for AngelHacks Toronto was done
+								by a student team of 4, which I was a major contributor of. This event was presented
+								by Hack Club, a global network of high school programmers and clubs.
+							</p>
+						</div>
+					</div>
+
+					<!-- Links -->
+					<div
+						class="text-lg font-medium mt-auto pt-10 w-full grid grid-flow-col text-center gap-x-4"
+					>
+						<a
+							href="https://angelhacksto.hackclub.com/"
+							target="_blank"
+							class="bg-[#FDB32B] py-4 rounded-xl">Visit Website</a
+						>
+						<a
+							href="https://bank.hackclub.com/angelhacks-toronto"
+							target="_blank"
+							class="bg-[#FDB32B] py-4 rounded-xl">See Finances</a
+						>
+					</div>
+				</div>
+
+				<!-- Media -->
+				<div class="grid-cols-2 hidden xl:grid">
+					<img
+						height="225"
+						width="300"
+						src="/projects/angelhacks-toronto/1.webp"
+						alt="Participants coding in the the main room."
+						class="rounded-xl overflow-hidden -rotate-1"
+						loading="lazy"
+					/>
+					<div />
+					<div />
+					<img
+						height="225"
+						width="300"
+						src="/projects/angelhacks-toronto/2.webp"
+						alt="A whiteboard showing brainstorming and planning for a game."
+						class="-mt-20 -ml-8 rounded-xl overflow-hidden rotate-1"
+						loading="lazy"
+					/>
+					<img
+						height="225"
+						width="300"
+						src="/projects/angelhacks-toronto/3.webp"
+						alt="All participants at the end of the event."
+						class="-mt-20 rounded-xl overflow-hidden rotate-1"
+						loading="lazy"
+					/>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
