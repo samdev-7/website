a0382a180f7d86c11b811bfdfd8f7edfaa1e6219
Sam Liu <hello@samliu.dev>
2024-05-23 22:29:26 -0400

    Let's start over

--- src/lib/stats-refresher.ts
+++
@@ -1,8 +0,0 @@
-import type { statsType } from "../routes/api/stats/+server";
-
-export async function refreshStats() {
-    const res = await fetch('/api/stats');
-    const result: statsType = await res.json();
-
-    return result;
-} 
