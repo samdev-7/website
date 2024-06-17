d4cfd031e730286169d4be6b05f33dfd653649f8
Sam Liu <hello@samliu.dev>
2024-06-16 23:34:55 -0400

    Fixes to image display in work

--- src/layouts/ProjectImages.svelte
+++ src/layouts/ProjectImages.svelte
@@ -30,7 +30,7 @@
     let reduceMotion = window.matchMedia(
       "(prefers-reduced-motion: reduce)"
     ).matches;
-    if (!reduceMotion) startAutoPlay();
+    if (!reduceMotion && numCards > 1) startAutoPlay();
   });
 
   function onScroll(
@@ -97,69 +97,71 @@
   >
     <slot />
   </div>
-  <button
-    class="absolute group left-0 top-0 bottom-0 flex items-center w-[30%]"
-    on:click={clickPrev}
-    title="Previous image"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke="currentColor"
-      class="size-6 sm:size-8 ml-1 sm:ml-4 text-white sm:text-transparent group-hover:text-white z-10 motion-safe:transition-colors motion-safe:duration-100"
+  {#if numCards > 1}
+    <button
+      class="absolute group left-0 top-0 bottom-0 flex items-center w-[30%]"
+      on:click={clickPrev}
+      title="Previous image"
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M15.75 19.5 8.25 12l7.5-7.5"
-      />
-    </svg>
-    <div
-      class="absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-r from-black/30 to-transparent to-70% -translate-x-full sm:group-hover:translate-x-0 motion-safe:transition-transform motion-safe:duration-100"
-    ></div>
-  </button>
-  <button
-    class="absolute group right-0 top-0 bottom-0 flex items-center w-[30%] justify-end"
-    on:click={clickNext}
-    title="Next image"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke="currentColor"
-      class="size-6 sm:size-8 mr-1 sm:mr-4 text-white sm:text-transparent group-hover:text-white z-10 motion-safe:transition-colors motion-safe:duration-100"
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        class="size-6 sm:size-8 ml-1 sm:ml-4 text-white sm:text-transparent group-hover:text-white z-10 motion-safe:transition-colors motion-safe:duration-100"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.75 19.5 8.25 12l7.5-7.5"
+        />
+      </svg>
+      <div
+        class="absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-r from-black/30 to-transparent to-70% -translate-x-full sm:group-hover:translate-x-0 motion-safe:transition-transform motion-safe:duration-100"
+      ></div>
+    </button>
+    <button
+      class="absolute group right-0 top-0 bottom-0 flex items-center w-[30%] justify-end"
+      on:click={clickNext}
+      title="Next image"
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="m8.25 4.5 7.5 7.5-7.5 7.5"
-      />
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        class="size-6 sm:size-8 mr-1 sm:mr-4 text-white sm:text-transparent group-hover:text-white z-10 motion-safe:transition-colors motion-safe:duration-100"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m8.25 4.5 7.5 7.5-7.5 7.5"
+        />
+      </svg>
 
-    <div
-      class="absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-l from-black/30 to-transparent to-70% translate-x-full sm:group-hover:translate-x-0 motion-safe:transition-transform motion-safe:duration-100"
-    ></div>
-  </button>
-  <div>
-    <div
-      class="absolute bottom-2 sm:bottom-3 left-0 right-0 flex justify-center space-x-1 sm:space-x-3 group"
-    >
-      {#each Array.from({ length: numCards }, (_, i) => i) as card}
-        <button
-          class="w-1.5 sm:w-2 h-1.5 sm:h-2 rounded-full hover:bg-white/80 motion-safe:transition-colors motion-safe:duration-100 {card ==
-          currentCard
-            ? 'bg-white'
-            : 'bg-white/50'}"
-          on:click={() => clickTo(card)}
-          title="Go to image {card + 1}"
-        ></button>
-      {/each}
+      <div
+        class="absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-l from-black/30 to-transparent to-70% translate-x-full sm:group-hover:translate-x-0 motion-safe:transition-transform motion-safe:duration-100"
+      ></div>
+    </button>
+    <div>
+      <div
+        class="absolute bottom-2 sm:bottom-3 left-0 right-0 flex justify-center space-x-1 sm:space-x-3 group"
+      >
+        {#each Array.from({ length: numCards }, (_, i) => i) as card}
+          <button
+            class="w-1.5 sm:w-2 h-1.5 sm:h-2 rounded-full hover:bg-white/80 motion-safe:transition-colors motion-safe:duration-100 {card ==
+            currentCard
+              ? 'bg-white'
+              : 'bg-white/50'}"
+            on:click={() => clickTo(card)}
+            title="Go to image {card + 1}"
+          ></button>
+        {/each}
+      </div>
     </div>
-  </div>
+  {/if}
 </div>
 
 <style>
