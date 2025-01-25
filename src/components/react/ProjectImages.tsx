b93a0718b32157247ecad32f4f517ececdf46712
Sam Liu <hello@samliu.dev>
2025-01-25 00:43:40 -0500

    Increase project image slideshow delay

--- src/components/react/ProjectImages.tsx
+++ src/components/react/ProjectImages.tsx
@@ -24,7 +24,7 @@ const ProjectImages: React.FC<ProjectImagesProps> = ({
     }
     timeoutRef.current = setInterval(() => {
       scrollNext();
-    }, 2000);
+    }, 10000);
   };
 
   useEffect(() => {
